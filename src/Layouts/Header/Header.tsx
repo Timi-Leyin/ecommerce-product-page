6d7599f5d8fb62b3b6b8999357d005a2c7ea4d9c
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-06-24 21:58:48 +0100

    Implemented Menu on Mobile view

--- src/Layouts/Header/Header.tsx
+++ src/Layouts/Header/Header.tsx
@@ -1,5 +1,7 @@
 import './header.scss';
 import logo from '../../Assets/images/logo.svg';
+import menu from '../../Assets/images/icon-menu.svg';
+import iconClose from '../../Assets/images/icon-close.svg';
 import avatar from '../../Assets/images/image-avatar.png';
 import { Dispatch, SetStateAction, useState } from 'react';
 import Cart from '../../Components/Cart/Cart';
@@ -7,16 +9,21 @@ import { TypeCart } from '../../App';
 // import { CartToggler } from '../../Components/Cart/Cart';
 
 const Header = ({cart,setCart}:{cart:TypeCart,setCart:Dispatch<SetStateAction<TypeCart>>}) => {
+  const [open,setMenu] = useState(false)
   return (
     <div id="header">
 <div className="header-main">
 
 
 <div className="header_logo">
+  <span className='menu' onClick={()=> setMenu(true)}><img src={menu} alt="" className='cursor-pointer'/></span>
   <img src={logo} alt="Logo" className="logo" />
 </div>
 
-<nav className="header_nav">
+<nav className={`header_nav ${open && 'open'}`}>
+  <div className="close cursor-pointer" onClick={()=> setMenu(false)}>
+    <img src={iconClose} alt="" />
+  </div>
 <ul className="nav-links">
   <li className="nav-link"><a href="/">Collections</a></li>
   <li className="nav-link active"><a href="/">Men</a></li>
