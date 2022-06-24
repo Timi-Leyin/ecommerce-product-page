d58e694d72e45faa250de55e828a529f2c38483a
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-06-24 18:29:23 +0100

    Created Gallery slide

--- src/Components/Product/Product.tsx
+++ src/Components/Product/Product.tsx
@@ -1,15 +1,24 @@
+import { useEffect, useState } from 'react';
 
 import './product.scss';
 import iconPlus from '../../Assets/images/icon-plus.svg';
 import iconMinus from '../../Assets/images/icon-minus.svg';
 import iconCart from '../../Assets/images/icon-cart.svg';
+import iconNext from '../../Assets/images/icon-next.svg';
+import iconPrevious from '../../Assets/images/icon-previous.svg';
 
-import { useState } from 'react';
+// slide images
+import productImage1 from '../../Assets/images/image-product-1.jpg'
+import productImage2 from '../../Assets/images/image-product-2.jpg'
+import productImage3 from '../../Assets/images/image-product-3.jpg'
+import productImage4 from '../../Assets/images/image-product-4.jpg'
+
+
 
 
 type QuantityFn = (type:string)=> void
 
-
+type ImageSlide = ()=> void
 
 const ProductQuantity=()=>{
     let [quantity,changeQuantity] = useState(0);
@@ -67,15 +76,63 @@ export const ProductInfo = () => {
 
 // PRODUCT DISPLAY
 export const ProductDisplay = () => {
+    // slide images ====> array
+    const images = [productImage1,productImage2,productImage3,productImage4];
+    // current image display index
+    const [index,changeIndex] = useState(0)
+
+    const max = images.length -1
+    const min = 0
+
+    const nextImage:ImageSlide = ()=> {
+        if(index >= images.length-1){
+            changeIndex(images.length-1)
+            return
+        }
+
+        changeIndex(index+1)
+
+    }
+
+    const prevImage:ImageSlide = ()=>{
+        if(index < 1){
+            changeIndex(0) 
+            return
+        }
+        changeIndex(index-1)
+    
+    }
+
+    useEffect(()=>{
+        console.log(index,'slide no')
+    },[index])
+
     return (
       <div className="product_display">
           <div className="gallery">
-              <div className="current-display"></div>
+
+              <div className="current-display">
+                  <img src={images[index]} alt="" className="img" />
+              <div className="control control-prev" style={{
+                  opacity:index==min ? 0.4 : 1
+              }} onClick={prevImage}><img src={iconPrevious} alt="previous" /></div>
+              <div className="control control-next"  style={{
+                  opacity:index==max ? 0.4 : 1
+              }}  onClick={nextImage}><img src={iconNext} alt="next" /></div>
+              </div>
+
               <div className="thumbnails">
-                  <div className="thumb"></div>
-                  <div className="thumb"></div>
-                  <div className="thumb"></div>
-                  <div className="thumb"></div>
+                 
+                  {
+                      images.map((img,i)=>{
+                          return(
+                            <div className={`thumb ${i == index && 'active'}`} key={i}  onClick={()=> changeIndex(i)}>
+                            <img src={images[i]} alt="" />
+                        </div>
+                          )
+                      })
+                  }
+               
               </div>
           </div>
       </div>
